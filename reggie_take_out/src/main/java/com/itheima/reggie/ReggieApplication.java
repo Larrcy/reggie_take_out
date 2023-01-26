660ae7d116d7e5d0600006d2d44931e349736a59
root <841407722@qq.com>
2023-01-26 14:07:03 +0800

    Redis清理缓存数据

--- reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -3,19 +3,19 @@ package com.itheima.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
+@EnableCaching
 @SpringBootApplication
-//@ServletComponentScan扫描过滤器文件创建过滤器
 @ServletComponentScan
-@EnableTransactionManagement
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
-        log.info("连接成功");
+        log.info("项目启动成功...");
     }
-
-
 }
